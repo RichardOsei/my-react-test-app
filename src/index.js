1681b3f1ff9fcb33aee7477299a1d00c616060b1
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2022-08-04 05:52:07 +0100

    Updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
+class Car {
+    constructor(name,year) {
+      this.brand = name;
+      this.year = year;
+    }
+  
+    present() {
+      return 'I have a ' + this.brand;
+    }
+  }
+  
+  class Model extends Car {
+    constructor(name,year, mod) {
+      super(name,year);
+      this.model = mod;
+    }  
+    show = () => this.present() + ', it is a ' + this.model +' in ' + this.year 
+    
+  }
+  
+  const mycar = new Model("Ford", "Mustang","2022");
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(mycar.show());
