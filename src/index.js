32b377a53f62232a2efc27834a65a9bb231ae5fc
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2022-08-05 20:21:13 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class Car {
+/*class Car {
     constructor(name,year) {
       this.brand = name;
       this.year = year;
@@ -21,12 +21,40 @@ class Car {
   }
   
   const mycar = new Model("Ford", "Mustang","2022");
+  
   const myArray = ['apple', 'banana', 'orange'];
+  const thecar = {type:"Fiat", model:"500", color:"white"};
 
   //map function
   const mylist = myArray.map((item) => <p>{item}</p>)
- 
+
+  const vehicles = ['mustang', 'f-150', 'expedition'];
+
+  const [Name,Num,paint] = thecar
+  
+ */
+
+  const person = {
+    name: 'Jesse',
+    age: 30, 
+    address: {
+      city: 'Houston',
+      state: 'Texas',
+      country: 'USA'
+    }
+  }
+  
+  displayMessage(person)
+  
+  function displayMessage({name,address: { city,state }}) {
+    return 'I am ' +name +' I live in ' +city+' ' + state + '.';
+  }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(mylist);
+//document.getElementById("demo").style.color = 'red';
+document.getElementById("demo").style.margin = '50%';
+
+root.render("WORKED");
+document.getElementById("demo").innerHTML = "worked";
+
